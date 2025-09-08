1b9e12878ddc7ae742a482577f8ecfa4a08da204
Ravshan <ravshanyusupov02@mail.ru>
2025-09-08 17:02:11 +0300

    restructured

--- app/main.go
+++ app/main.go
@@ -43,48 +43,32 @@ func main() {
 }
 
 func matchLine(line []byte, pattern string) (bool, error) {
-
-	if pattern == "\\d" {
-		return bytes.ContainsAny(line, "0123456789"), nil
-	} else if pattern == "\\w" {
-		isAlphanum := func(r rune) bool {
-			return r >= 'a' && r <= 'z' || r >= 'A' && r <= 'Z' || r >= '0' && r <= '9' || r == '_'
-		}
-		return bytes.ContainsFunc(line, isAlphanum), nil
-	} else if pattern[0] == '[' && pattern[len(pattern)-1] == ']' {
-		if pattern[1] == '^' {
-			counter := 0
-			for _, v := range line {
-				for _, w := range pattern[2 : len(pattern)-1] {
-					if rune(v) == w {
-						counter++
-					}
-				}
-			}
-			fmt.Println("line", line, len(line))
-			fmt.Println("counter:", counter)
-			if counter != len(line) {
-				return true, nil
-			}
-			return false, nil
-
-		} else {
-			letters := pattern[1 : len(pattern)-1]
-			return bytes.ContainsAny(line, letters), nil
-		}
-	}
-
 	if utf8.RuneCountInString(pattern) != 1 {
 		return false, fmt.Errorf("unsupported pattern: %q", pattern)
 	}
 
 	var ok bool
 
+	if pattern == "\\d" {
+		ok = bytes.ContainsAny(line, "0123456789")
+	} else if pattern == "\\w" {
+		isAlphanum := func(r rune) bool {
+			return r >= 'a' && r <= 'z' || r >= 'A' && r <= 'Z' || r >= '0' && r <= '9' || r == '_'
+		}
+		ok = bytes.ContainsFunc(line, isAlphanum)
+	} else if pattern[0] == '[' && pattern[len(pattern)-1] == ']' {
+		if pattern[1] == '^' {
+			ok = !bytes.ContainsAny(line, pattern[2:len(pattern)-1])
+		} else {
+			ok = bytes.ContainsAny(line, pattern[1:len(pattern)-1])
+		}
+	}
+
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
 
 	// Uncomment this to pass the first stage
-	ok = bytes.ContainsAny(line, pattern)
+	//ok = bytes.ContainsAny(line, pattern)
 
 	return ok, nil
 }
